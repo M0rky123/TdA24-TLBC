c3bf731d30b849fd2d0aac1219da28172d1dc83b
M0rky123 <smutny.vojta@gmail.com>
2024-02-21 16:12:55 +0100

    vyjebanej post pico, kdo mi to zas dovolil (ja)

--- frontend/app/components/Cards.tsx
+++ frontend/app/components/Cards.tsx
@@ -1,15 +1,56 @@
+import { Key } from "react";
+import { fetchLecturerPack } from "../utils/fetch";
 import Card from "./Card";
 import style from "@/app/styles/Cards.module.css";
 
-export default function Cards() {
+interface CardsProps {
+  page: number;
+  locArray: string[];
+  tagArray: string[];
+  priceArray: number[];
+}
+
+export default function Cards({ page, locArray, tagArray, priceArray }: CardsProps) {
+  async function getLecturers() {
+    const data = await fetchLecturerPack(page, 8);
+    return data;
+  }
+
   return (
     <section className={style.cards}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {getLecturers().then((data) =>
+        data.forEach(
+          (lecturer: {
+            uuid: Key | null | undefined;
+            first_name: string;
+            last_name: string;
+            claim: string | undefined;
+            location: string | undefined;
+            middle_name: string | undefined;
+            picture_url: string | undefined;
+            price_per_hour: number | undefined;
+            tags: string[] | undefined;
+            title_after: string | undefined;
+            title_before: string | undefined;
+          }) => {
+            return (
+              <Card
+                key={lecturer.uuid}
+                first_name={lecturer.first_name}
+                last_name={lecturer.last_name}
+                claim={lecturer.claim}
+                location={lecturer.location}
+                middle_name={lecturer.middle_name}
+                picture_url={lecturer.picture_url}
+                price_per_hour={lecturer.price_per_hour}
+                tags={lecturer.tags}
+                title_after={lecturer.title_after}
+                title_before={lecturer.title_before}
+              />
+            );
+          }
+        )
+      )}
     </section>
   );
 }
--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -2,7 +2,7 @@ import { openSans } from "./data/fonts";
 
 export default function NotFount() {
   return (
-    <div className={openSans} style={{ margin: "auto", fontSize: "1.5rem", fontWeight: "bold" }}>
+    <div className={openSans} style={{ margin: "auto", fontSize: "1.5rem", fontWeight: "bold", color: "var(--black)" }}>
       Stránka kterou hledáte nebyla nalezena...
     </div>
   );
