d0603c2a8d14dafde47428dcbbbc48427e36e803
TheKronis <73104361+TheKronis@users.noreply.github.com>
2024-02-21 22:40:17 +0000

    Update start.sh script to run frontend build process in the background

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+
+
+# Start the frontend build process
+cd ./frontend
+npm install
+npm run build 
+npm run start &
+
 # Move back to the backend directory
-cd ./backend
+cd ../backend
 
 # Initialize the database in the background
 python3 -m flask --app app/app.py init-db &
 
 # Run Flask using Gunicorn in the foreground to keep the container alive
-gunicorn --bind 0.0.0.0:80 app.app:app &
-
-# Start the frontend build process
-cd ../frontend
-npm install
-npm run build 
-npm run start 
-
+gunicorn --bind 0.0.0.0:80 app.app:app 
