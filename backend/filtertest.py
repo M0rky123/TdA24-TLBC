2f8b5278ede93cb0e9ba858c6c43fe0393a20d78
TheKronis <73104361+TheKronis@users.noreply.github.com>
2024-02-29 07:46:04 +0000

    meow :3

--- backend/filtertest.py
+++ backend/filtertest.py
@@ -2,7 +2,7 @@ from flask import jsonify
 import requests
 import json
 
-url = "http://127.0.0.1/api/reserve/934f7782-b7fe-46a9-9e4d-4c37a0cd9710"
+url = "http://127.0.0.1:8080/api/reserve/934f7782-b7fe-46a9-9e4d-4c37a0cd9710"
 url_get_all = "http://127.0.0.1:5000/api/lecturers"
 #url = "http://f7b5d5a152c395f1.app.tourdeapp.cz/api/lecturers/999a8b5f-8dda-4ba8-8a15-21df6110f573"
 #Need to force commit lol
