c7449eae0c625b8640606c199be6d1b0b90d7ebb
TheKronis <73104361+TheKronis@users.noreply.github.com>
2024-03-01 21:23:13 +0000

    ztgf

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ npm run start &
 
 # Start the backend on a different port
 cd ../backend
-python3 -m flask --app app/app.py init-db &
-gunicorn --bind 0.0.0.0:5000 app.app:app &
+python3 -m flask --app app/app.py init-db
+gunicorn --bind 127.0.0.1:5000 app.app:app &
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
