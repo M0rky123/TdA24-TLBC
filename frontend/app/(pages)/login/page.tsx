3ed978d379d2d5cf9bbbeeb11b1922fe47e64487
TheKronis <73104361+TheKronis@users.noreply.github.com>
2024-03-01 20:32:48 +0000

    Update API endpoint and fix fetch function

--- frontend/app/(pages)/login/page.tsx
+++ frontend/app/(pages)/login/page.tsx
@@ -15,7 +15,7 @@ export default function Login() {
   const route = useRouter();
 
   async function handleLogin(username: string, password: string) {
-    await fetch("http://localhost/api/auth", {
+    await fetch("/api/auth", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name: username, password: password }),
